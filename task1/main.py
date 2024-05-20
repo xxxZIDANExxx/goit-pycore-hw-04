64997ca2f066fb14aee5c54558ae5779bc482ab4
mykola.mazo.92@gmail.com <mykola.mazo.92@gmail.com>
2024-05-20 20:45:00 +0300

    task1

---
+++ task1/main.py
@@ -0,0 +1,42 @@
+"""Function read_data"""
+def read_data(path:str) -> list[str] | None:
+    """Open file"""
+    try:
+        with open(path, "r", encoding="UTF-8") as salaries_file:
+            salaries = list()
+            for line in salaries_file.readlines():
+                salaries.append(line.strip())
+            return salaries
+    except FileNotFoundError as fnf:
+        print(fnf)
+        print("File was not found")
+        return None
+    except ValueError as ve:
+        print(ve)
+        print("Incorrect data in the file")
+        return None
+"""Module providing a function printing python version."""
+def process_salaries(dev_salaries:list[str]) -> tuple[int, int] | tuple[None, None]:
+    salaries=list()
+    for dev_salary in dev_salaries:
+        _, salary = dev_salary.split(",")
+        salary = int(salary)
+        salaries.append(salary)
+    if salaries:
+        total_sum = sum(salaries)
+        avg = total_sum/len(salaries)
+        return (total_sum, avg)
+    else:
+        return (None, None)
+"""Module providing a function printing python version."""
+def total_salary(path:str) -> tuple[int, int]:
+    return process_salaries(read_data(path))
+
+"""Module providing a function printing python version."""
+def main():
+    total, average = total_salary("test.txt")
+    print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+
+
+if __name__ == "__main__":
+    main()
